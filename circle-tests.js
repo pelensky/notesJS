c4a89747b7749bdb89541f78a4a60dd3f2e25839
sara <sara@huhbub.com>
2017-01-04 15:41:04 +0000

    removed pre-project test files

--- circle-tests.js
+++
@@ -1,9 +0,0 @@
-function testCircleRadiusDefaultsTo10() {
-  var circle = new Circle();
-
-  if (circle.radius() !== 10) {
-    throw new Error("Circle size is not 10");
-  }
-};
-
-testCircleRadiusDefaultsTo10();
