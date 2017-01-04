65d9755b65f6b90ab0705ee81feaa35e7a84ff38
sara <sara@huhbub.com>
2017-01-04 18:21:46 +0000

    corrects errors in initial tests set up

--- assert.js
+++ assert.js
@@ -1,6 +1,5 @@
 var assert = {
   isTrue: function(assertionToCheck) {
-    isTrue: function(assertionToCheck) {
       if (!assertionToCheck) {
         throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
       }
